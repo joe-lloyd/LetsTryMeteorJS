f55b34f7df42917196c9f9d162a9646bfd2e6625
Joe Lloyd <joe.lloyd.22.24@gmail.com>
2015-08-24 14:17:32 +0100

    added Facebook login and basic Auth

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -14,6 +14,9 @@ if (Meteor.isClient) {
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -28,7 +31,9 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
  
       // Clear form
@@ -49,5 +54,19 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  Accounts.loginServiceConfiguration.remove({
+    service: "facebook"
+  });
+  
+  Accounts.loginServiceConfiguration.insert({
+    service: "facebook",
+    appId: "yourAppId",
+    secret: "yourSecret"
   });
 }
